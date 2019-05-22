07f8d0c7bc053d9c9507fd040bc2fc1d120e39c8
mathus101 <mattjamesbrooks@gmail.com>
2019-05-22 20:35:51 +1000

    Added interpolation demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: `<div>
+                  <h1>Angular</h1>
+                  <p>{{title}}</p>   <!-- Interpolation. title in DOM will be updated when it changes in the component
+                </div>
+                <div>
+                <button (click)="changeTitle()">Change Title in Component</button> `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'Angular-Antics';
+
+  changeTitle()
+  {
+    this.title = "New Title";
+  }
+
 }
